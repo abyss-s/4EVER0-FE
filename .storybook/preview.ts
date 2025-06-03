26b3c25ae1bd15ebafdf1651ab916a6deab7ca98
abyss-s <lh3729@naver.com>
2025-06-03 17:39:38 +0900

    Chore: [EVER-30] Storybook Update 8.6.14 -> 9.0.4

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { withThemeByClassName } from "@storybook/addon-themes";
 import "../src/index.css";
 
 const preview: Preview = {
@@ -8,6 +9,39 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      default: "light",
+      values: [
+        { name: "light", value: "#ffffff" },
+        { name: "dark", value: "#000000" },
+      ],
+    },
+  },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "", //
+        dark: "dark",
+      },
+      defaultTheme: "light",
+      parentSelector: "html",
+    }),
+  ],
+};
+
+export const globalTypes = {
+  theme: {
+    name: "Theme",
+    description: "Global theme for components",
+    defaultValue: "light",
+    toolbar: {
+      icon: "mirror",
+      items: [
+        { value: "light", icon: "sun", title: "Light Mode" },
+        { value: "dark", icon: "moon", title: "Dark Mode" },
+      ],
+      showName: true,
     },
   },
 };
