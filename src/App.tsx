eee8f1bfbbf56f502aa4cc32a7bed89147fee1b7
abyss-s <lh3729@naver.com>
2025-06-07 19:55:57 +0900

    Feat: [EVER-78] 디자인 시스템 초기 설정
    - Pretendard 폰트 설정
    - 브랜드 및 상태용 색상 추가
    - 다크모드 제어를 위한 ThemeProvider 전역 설정
    
    Co-authored-by: EUNCHAE LEE <eunchrri@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 
-function App() {
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
