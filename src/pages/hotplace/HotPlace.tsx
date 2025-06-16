1cfd2c02e32fc6bfa70de8cbd88c55e363b898b3
abyss-s <lh3729@naver.com>
2025-06-17 01:51:44 +0900

    Feat: [EVER-173] 기존 헤더 추가

--- src/pages/hotplace/HotPlace.tsx
+++ src/pages/hotplace/HotPlace.tsx
@@ -3,7 +3,15 @@ import PopupMap from './PopupMap/PopupMap';
 const HotPlace: React.FC = () => {
   return (
     <div>
+      <div className="flex justify-center pb-4">
+        <div className="bg-gray-800 text-white px-4 py-2 rounded-full text-sm">
+          ⭐ 요즘 핫한 MZ들의 PICK은?!
+        </div>
+      </div>
+
       <PopupMap />
+
+      {/* BEST 혜택 섹션 */}
     </div>
   );
 };
