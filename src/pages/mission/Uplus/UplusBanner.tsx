2e8b74e8526758adeaa6a208be3faad50a4d1b50
eunchrri <leesilver7@gmail.com>
2025-06-22 21:00:39 +0900

    Feat: [EVER-236] 유플투쁠 배너 description 수정

--- src/pages/mission/Uplus/UplusBanner.tsx
+++ src/pages/mission/Uplus/UplusBanner.tsx
@@ -8,7 +8,9 @@ export const UplusBanner = () => {
       size="sm"
       layout="default"
       title="매일 누리는 특별 혜택! 🎁🎉"
-      description="지금 바로 확인하세요 🐙"
+      description={
+        '유플투쁠, 지금 바로 확인하세요 🐙 \n 날짜를 클릭하면 상세 혜택을 확인할 수 있어요!'
+      }
       image={IMAGES.MOONER.MOONER_PRESENT}
       className="mb-6"
     />
