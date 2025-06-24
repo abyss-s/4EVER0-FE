d197990c74e86f36999b624b61aad494d8d58cd7
jihoi0615 <pjhalice0615@gmail.com>
2025-06-25 00:17:26 +0900

    Refactor: [EVER-270] 전체 dev 주석 클린업 #218

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,10 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-// import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    // visualizer({ open: true })
-  ],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
