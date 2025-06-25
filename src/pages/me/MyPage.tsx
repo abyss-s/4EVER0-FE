787dfde56bf9cc5a1d96a0c20afd13d9b3786f2a
illustermin <2002hongmj@naver.com>
2025-06-25 21:28:30 +0900

    Refactor: [EVER-281] moonerzone 수정

--- src/pages/me/MyPage.tsx
+++ src/pages/me/MyPage.tsx
@@ -197,7 +197,7 @@ const MyPage: React.FC = () => {
                 <div className="relative">
                   <div className="w-full h-4 bg-white rounded-full overflow-hidden border border-brand-yellow">
                     <div
-                      className="h-full bg-gradient-to-r from-brand-yellow to-brand-yellow-hover rounded-full transition-all duration-500 ease-out relative"
+                      className="h-full bg-gradient-to-r from-brand-yellow to-[#f94941] rounded-full transition-all duration-500 ease-out relative"
                       style={{ width: `${(profile.point / 5000) * 100}%` }}
                     >
                       <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white/30 to-transparent"></div>
