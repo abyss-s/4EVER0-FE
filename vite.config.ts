ab997e641f9b57254f3dce4509b83764e042001d
jihoi0615 <pjhalice0615@gmail.com>
2025-06-25 09:37:34 +0900

    Feat: [EVER-270] vite.config.ts 롤백 #219

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  // visualizer({ open: true })
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
