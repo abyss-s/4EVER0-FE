f4a8e0e242b73d8e151f24a68ec72b59ae98b58a
Kyonyoung Park <rysud0125@gmail.com>
2025-06-27 09:44:39 +0900

    hotfix: Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { GlobalModalProvider } from './provider/GlobalModalProvider/GlobalModalProvider';
 import { Sooner } from '@/components/Sooner/Sonner';
